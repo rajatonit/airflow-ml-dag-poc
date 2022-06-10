83aaa8030e20f83eb9d00a06bbe5a41fa5043aa3
Rajat Arora <rajat.arora@hotmail.ca>
2022-06-10 11:35:25 -0400

    added spark-kub ex

--- dags/example_spark_kubernetes_operator.py
+++ dags/example_spark_kubernetes_operator.py
@@ -48,7 +48,7 @@ default_args = {
 # [START instantiate_dag]
 
 
-with DAG('submit spark-pi as sparkApplication on kubernetes', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
+with DAG('submit-spark-pi-ex', schedule_interval='@daily', default_args=default_args, catchup=False) as dag:
     t1 = SparkKubernetesOperator(
         task_id='spark_pi_submit',
         namespace="default",
